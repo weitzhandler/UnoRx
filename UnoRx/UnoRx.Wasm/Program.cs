3591744972d7281d4b0d177b44f8812332635dbb
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2019-09-25 05:01:09 +0300

    Add project files.

---
+++ UnoRx/UnoRx.Wasm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.PlatformServices;
+using Windows.UI.Xaml;
+
+namespace UnoRx.Wasm
+{
+  public class Program
+  {
+    private static App _app;
+
+    static int Main(string[] args)
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+      PlatformEnlightenmentProvider.Current.EnableWasm();
+#pragma warning restore CS0618 // Type or member is obsolete
+
+      Windows.UI.Xaml.Application.Start(_ => _app = new App());
+
+      return 0;
+    }
+  }
+}
